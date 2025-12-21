d289e08a39ebcfc7ae66ab6f1e05afa1e12a81ee
SergeiVS <fam.smirnoff@gmail.com>
2025-12-21 15:09:22 +0100

    add navigation buttons to search results

--- src/app/pages/search-results/search-results.component.ts
+++ src/app/pages/search-results/search-results.component.ts
@@ -2,12 +2,15 @@ import { Component, linkedSignal, OnInit } from '@angular/core';
 import { ResaultsTableComponent } from 'src/app/components/resaults-table/resaults-table.component';
 import { DataFilterService } from 'src/app/service/google-service/data-filter.service';
 import { ModalService } from 'src/app/service/modal.service';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
-  imports: [ResaultsTableComponent],
+  imports: [ResaultsTableComponent, MatIcon, MatIconButton],
   standalone: true,
 })
 export class SearchResultsComponent implements OnInit {
@@ -15,7 +18,8 @@ export class SearchResultsComponent implements OnInit {
 
   constructor(
     private filterService: DataFilterService,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -26,5 +30,12 @@ export class SearchResultsComponent implements OnInit {
         'Für gegebenen Zeitraum werden keine Werte gespeichert'
       );
     }
+  }
+
+  navigateHome() {
+    this.router.navigate(['/home']);
+  }
+  navigateBack() {
+    this.router.navigate(['/statistics']);
   }
 }
