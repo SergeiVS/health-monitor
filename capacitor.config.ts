fbe938efd828773e8604ce6a086ab042a995e73b
SergeiVS <fam.smirnoff@gmail.com>
2026-01-11 15:38:34 +0100

    Update Dependencies

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'health-monitor',
-  webDir: 'www'
+  webDir: 'www/en'
 };
 
 export default config;
