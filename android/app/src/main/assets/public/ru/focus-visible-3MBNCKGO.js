5c934c7fd39ac9ada615a18d7f1311ed27343e9a
SergeiVS <fam.smirnoff@gmail.com>
2026-01-12 18:31:14 +0100

    update android settings

--- android/app/src/main/assets/public/ru/focus-visible-3MBNCKGO.js
+++
@@ -1,9 +0,0 @@
-import {
-  startFocusVisible
-} from "./chunk-VBX7D7XX.js";
-import "./chunk-4RK3B3NN.js";
-export {
-  startFocusVisible
-};
-/**i18n:47b311c64cc08524d625bf337677bb5214eca088ef5369afdb0896055efe48a1*/
-//# sourceMappingURL=focus-visible-3MBNCKGO.js.map
